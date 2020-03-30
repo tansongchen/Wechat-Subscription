3c2e33b79efe271f88d308ee325911f8f3734533
tansongchen <tansongchen@pku.edu.cn>
2020-03-30 08:24:19 +0800

    Markdown Nice 更新补丁

--- CSS/生成.py
+++ CSS/生成.py
@@ -1,15 +1,15 @@
-f = open('样式.css', encoding='utf-8', mode='r')
+f = open('CSS/样式.css', encoding='utf-8', mode='r')
 styleStringWithoutColor = f.read()
 f.close()
 
-f = open('配色.json', encoding='utf-8', mode='r')
+f = open('CSS/配色.json', encoding='utf-8', mode='r')
 schemeDict = eval(f.read())
 f.close()
 
 typoraPath = '/Users/tansongchen/Library/Application Support/abnerworks.Typora/themes/'
 typoraNameList = ['wechat-red', 'wechat-orange', 'wechat-yellow', 'wechat-green', 'wechat-aqua', 'wechat-blue', 'wechat-purple']
 
-f = open('Typora.css', encoding='utf-8', mode='r')
+f = open('CSS/Typora.css', encoding='utf-8', mode='r')
 typoraString = f.read()
 f.close()
 typoraGlobalString = """
@@ -26,7 +26,7 @@ for scheme, colorsDict in schemeDict.items():
     styleString = styleStringWithoutColor
     for colorType, colorValue in sorted(colorsDict.items(), reverse=True):
         styleString = styleString.replace(colorType, colorValue)
-    f = open('生成/%s.css' % scheme, encoding='utf-8', mode='w')
+    f = open('CSS/生成/%s.css' % scheme, encoding='utf-8', mode='w')
     f.write(styleString)
     f.close()
     f = open(typoraPath + '%s.css' % typoraName, encoding='utf-8', mode='w')
